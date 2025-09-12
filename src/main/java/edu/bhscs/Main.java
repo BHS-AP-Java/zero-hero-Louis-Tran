fb3297c85aeaae6bb0dfa290dc15798b53f3b851
Louis Tran <2035426@apps.nsd.org>
2025-09-12 16:46:33 +0000

    Made the computer println

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,19 @@
-// Student name
-// class period
-// project name
-// date
+// Louis Tran
+// Period 2
+// Zero Hero
+// 9/12/25
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr. Reiber says
+ * INPUT: We don't know about input yet
+ * OUTPUT: Some cool words in the command line terminal thing
+ * EDGE CASE: They are all edge cases at the moment
  */
 
 package edu.bhscs;
+
+class Main{
+  public static void main(String[] args) {
+    System.out.println("Sigma");
+  }
+}
