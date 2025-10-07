af9a07efecdf2b7a423f167dff6333a277217ec8
Louis Tran <2035426@apps.nsd.org>
2025-10-07 17:05:42 +0000

    fixed the interaction

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Baker Bob = new Baker(new Player("Bob"), "Bob");
         Bob.takeJob(new Store("Bothell", "Bob's Bakery"));
-
-        Bob.takeOrder(10, new Customer("Alice", 25));
-        System.out.println(Bob.cash);
+        Customer Alice = new Customer("Alice", 25);
+        Bob.takeOrder(10, Alice);
+        Alice.eatCake();
 
     }
 
