2235b290c485e04c81e209821327726cde1f92fb
Louis Tran <2035426@apps.nsd.org>
2025-10-14 16:51:33 +0000

    testing in main.java

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -11,6 +11,14 @@ package edu.bhscs;
 
 public class Main {
   public static void main(String[] args) {
-    new Cake("Chocolate", 3, new Flour(8, "All purpose")).draw();
+    // Create a player
+    Player p = new Player("Louis Tran");
+    Store bakery = new Store("Bothell", "Sweet Treats Bakery");
+    Baker bob = new Baker(p, "Bob");
+    bob.takeJob(bakery);
+    Customer alice = new Customer("Alice", 50);
+    bob.takeOrder(20, alice);
+
+    alice.eatCake();
   }
 }
