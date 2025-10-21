d760103c7f35ff846efa221573b249c50af6274d
Louis Tran <2035426@apps.nsd.org>
2025-10-21 16:26:54 +0000

    I proved I understand how to write the app file.

--- src/main/java/edu/bhscs/App.java
+++ src/main/java/edu/bhscs/App.java
@@ -2,6 +2,12 @@ package edu.bhscs;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+
+    date_10_21();
+    System.out.println("I am the app");
+  }
+
+  public static void date_10_21() {
+    System.out.println("Today's work");
   }
 }
