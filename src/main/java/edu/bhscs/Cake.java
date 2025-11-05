73d500b7055b852e9eeb2e057f940c898e8aaa76
Louis Tran <2035426@apps.nsd.org>
2025-11-05 17:38:56 +0000

    made the draw both function differentiate between which is bigger.

--- src/main/java/edu/bhscs/Cake.java
+++ src/main/java/edu/bhscs/Cake.java
@@ -30,10 +30,30 @@ public class Cake {
   }
 
   // Draws a 3D ASCII art of a cake based on the number of layers
+  void draw() {
+
+
+
+    System.out.println("                       ________");
+    System.out.println("       __....----''''''        ```````----....__");
+    System.out.println("  _-'''                                         ```-_");
+    System.out.println(".'" + colorCake("frosting", 51) + "`.");
+    for (int i = 0; i < this.layers; i++) {
+      System.out.println("|`-_" + colorCake(this.flavor, 47) + "_-'|");
+      System.out.println("|   ```--....____                     ____....--'''   |");
+      System.out.println("|                `````-----------'''''                |");
+      if (i == this.layers - 1) {
+        System.out.println(" `-_" + colorCake(this.flavor, 47) + "_-'");
+        System.out.println("    ```--....____                     ____....--'''");
+        System.out.println("                 `````-----------'''''");
+      }
+    }
+  }
+
   void draw(int offset) {
-    String gap="";
-    for(int i=0; i<offset; i++) {
-      gap+=" ";
+    String gap = "";
+    for (int i = 0; i < offset; i++) {
+      gap += " ";
     }
 
     System.out.print(gap);
@@ -68,9 +88,15 @@ public class Cake {
     if( 55-t.width <0 ) {
       //table is bigger than cake
       offset = Math.abs(55 - t.width)/2;
+      this.draw(offset);
+      t.draw();
     }
-    this.draw(offset);
-    t.draw();
+    else {
+      //cake is bigger than table
+      this.draw();
+      t.draw(offset);
+    }
+
   }
 
   String colorCake(String middle, int gapLength) {
--- src/main/java/edu/bhscs/Table.java
+++ src/main/java/edu/bhscs/Table.java
@@ -60,4 +60,45 @@ public class Table {
       System.out.println(tableLegs);
     }
    }
-}
+
+   void draw(int offset) {
+     // There are 8 spaces at the start of the cake, with 55 characters
+
+      int cakeWidth = 55; int widthFiller = (cakeWidth - this.width) / 2;
+      // Draw top of table
+      String fillerGap = "";
+        for(int i = 0; i < widthFiller ; i++){
+          fillerGap += " "; }
+      System.out.print(fillerGap);
+
+
+    String widthGap = "";
+    for (int i = 0; i < this.width; i++) {
+       widthGap += "=";
+     }
+     System.out.println(widthGap);
+     // Draw legs of table
+     int legGaps = (this.width / this.legs);
+
+     String tableLegs = "";
+
+     for (int i = 0; i < this.legs; i++) {
+       if (i == 0) {
+         // first leg
+         for (int j = 0; j < ((legGaps / 2) - 1); j++) {
+           tableLegs += " ";
+         }
+       } else {
+         for (int j = 0; j < legGaps; j++) {
+           tableLegs += " ";
+         }
+       }
+       tableLegs += "|";
+     }
+
+     for (int i = 0; i < (this.height - 1); i++) {
+       System.out.print(fillerGap);
+       System.out.println(tableLegs);
+     }
+   }
+  }
